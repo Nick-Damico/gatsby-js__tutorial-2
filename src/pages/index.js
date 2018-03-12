85c5e0eda993ae40da7617cb8a14780ec15e64b6
Nicholas D'Amico <nickalan82@icloud.com>
2018-03-11 23:36:00 -0400

    add fairy-gates typography theme, add pages/ 'about,contact'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,26 +1,39 @@
 import React from "react";
 
+
+// UnComment this block and Comment out all other code after this block to see results.
+  // export default () => (
+  //   <div className="blog-post" style={{ margin: '3rem auto', maxWidth: 600 }}>
+  //     <h1 className="blog-post__title">Richard Hamming on Luck</h1>
+  //     <div>
+  //       <p className="blog-post__text">
+  //         From Richard Hamming’s classic and must-read talk, “<a className="blog-post__link" href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
+  //           You and Your Research
+  //         </a>”.
+  //       </p>
+  //       <blockquote className="blockquote">
+  //         <p className="blockquote__text">
+  //           There is indeed an element of luck, and no, there isn’t. The prepared
+  //           mind sooner or later finds something important and does it. So yes, it
+  //           is luck.{" "}
+  //           <em>
+  //             The particular thing you do is luck, but that you do something is
+  //             not.
+  //           </em>
+  //         </p>
+  //       </blockquote>
+  //     </div>
+  //     <p className="blog-post__text">Posted April 09, 2011</p>
+  //   </div>
+  // );
+
+
 export default () => (
-  <div className="blog-post" style={{ margin: '3rem auto', maxWidth: 600 }}>
-    <h1 className="blog-post__title">Richard Hamming on Luck</h1>
-    <div>
-      <p className="blog-post__text">
-        From Richard Hamming’s classic and must-read talk, “<a className="blog-post__link" href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
-          You and Your Research
-        </a>”.
-      </p>
-      <blockquote className="blockquote">
-        <p className="blockquote__text">
-          There is indeed an element of luck, and no, there isn’t. The prepared
-          mind sooner or later finds something important and does it. So yes, it
-          is luck.{" "}
-          <em>
-            The particular thing you do is luck, but that you do something is
-            not.
-          </em>
-        </p>
-      </blockquote>
-    </div>
-    <p className="blog-post__text">Posted April 09, 2011</p>
+  <div>
+    <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+    <p>
+      What do I like to do? Lots of course but definitely enjoy building
+      websites.
+    </p>
   </div>
-);
+)
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,7 +1,8 @@
 import Typography from 'typography';
 // import bootstrapTheme from "typography-theme-bootstrap";
-import lawtonTheme from 'typography-theme-lawton';
+// import lawtonTheme from 'typography-theme-lawton';
+import fairyGate from 'typography-theme-fairy-gates';
 
-const typography = new Typography(lawtonTheme);
+const typography = new Typography(fairyGate);
 
 export default typography;
